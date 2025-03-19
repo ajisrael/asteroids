6de4bb1cb817f5f5ce3a9b34c6674cb22ccdd166
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2025-03-19 15:52:36 -0400

    f   - created main file

---
+++ main.py
@@ -0,0 +1,14 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+
+# This line ensures the main() function is only called when this file is run directly;
+# it won't run if it's imported as a module. It's considered the "pythonic" way to structure
+# an executable program in Python. Technically, the program will work fine by just calling
+# main(), but you might get an angry letter from Guido van Rossum if you don't.
+if __name__ == "__main__":
+    main()

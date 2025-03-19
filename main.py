1f97852fa82ff352c3805d1429427a2e05dda11d
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2025-03-19 15:57:29 -0400

    f   - added constants and import requirements

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 # This line ensures the main() function is only called when this file is run directly;
 # it won't run if it's imported as a module. It's considered the "pythonic" way to structure
